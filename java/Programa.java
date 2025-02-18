9e9010dea7e61e5d64a9533a034bae0a4c9a88b9
mltomacheski <160253044+mltomacheski@users.noreply.github.com>
2025-02-18 11:14:54 -0300

    javinha

---
+++ java/Programa.java
@@ -0,0 +1,42 @@
+/*import java.util.Scanner;
+
+
+public class Programa {
+    public static void main ( String args[]){
+      
+       Scanner sc = new Scanner (System.in);
+        System.out.println("Por favor, digite o seu nome");
+
+        String nomePessoa = sc.nextLine();
+
+        System.out.println("Por favor, digite o número do seu cartão");
+        String numeroCartao = sc.nextLine();
+
+     System.out.println("============================");
+        System.out.println(nomePessoa);
+        System.out.println(numeroCartao);
+     System.out.println("============================");
+
+    }
+}*/
+
+import java.util.Scanner;
+
+public class Programa {
+    public static void main ( String args[]){
+      
+       Scanner sc = new Scanner (System.in);
+        System.out.println("Por favor, digite suas tres notas aqui ");
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número do seu cartão");
+        String numeroCartao = sc.nextLine();
+
+     System.out.println("============================");
+        System.out.println(nomePessoa);
+        System.out.println(numeroCartao);
+     System.out.println("============================");
+
+    }
+}
